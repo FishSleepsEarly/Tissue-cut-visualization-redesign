e70406024fa8671b34c25b3b423cbd3ac8e71865
Fin Dragon <qilongyu078@gmail.com>
2025-03-09 13:24:00 -0600

    Added new feature to project

--- src/three_window.js
+++ src/three_window.js
@@ -174,3 +174,5 @@ opacitySlider.addEventListener('input', (event) => {
 
 // Example: Convert disc at index 9 into a pie chart
 togglePieDisc(9, [0xff0000, 0x00ff00, 0x0000ff], [0.2, 0.4, 0.4]);
+
+// Test message
