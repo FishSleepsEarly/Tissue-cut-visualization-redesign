02ca5c43809afb2f9787d6d7ae5a285474a99954
Ruban Gino Singh <rubangino22@gmail.com>
2025-04-04 23:42:54 -0600

    FIX Tailwind page load

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
   // Configuration options go here
 
   // If your main entry file is in public/vtk_window.js:
   // (Though typically, you'd place your source in a `src/` folder)
   build: {
     rollupOptions: {
-      input: '/src/vtk_window.js', 
+      input: '/src/vtk_window.js',
       // or point to your main JS entry if you restructure
     },
     outDir: 'dist',  // The folder where production build files will go
